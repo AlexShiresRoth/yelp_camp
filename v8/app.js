d6fda79c7f83e1b1e20430b7dc7c219b48168441
Alex Roth <alexroth96@gmail.com>
2018-08-12 01:59:40 +0000

    add v12deployed

--- v8/app.js
+++ v8/app.js
@@ -2,10 +2,8 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
-    flash = require('connect-flash'),
     passport = require('passport'),
     LocalStrategy = require('passport-local'),
-    methodOverride = require('method-override'),
     Campground = require('./models/campground'),
     Comment = require("./models/comment"),
     User = require('./models/user'),
@@ -16,13 +14,10 @@ var commentRoutes     = require('./routes/comments'),
     campgroundRoutes  = require('./routes/campgrounds'),
     indexRoutes       = require('./routes/index');
     
-    //mongoose.connect("mongodb://localhost/yelp_camp_v12");
-    mongoose.connect("mongodb://alex:jazzman89@ds117422.mlab.com:17422/yelpcamp");
+    mongoose.connect("mongodb://localhost/yelp_camp_v8");
     app.use(bodyParser.urlencoded({extended: true}));
     app.set("view engine", "ejs");
     app.use(express.static(__dirname + "/public"));
-    app.use(methodOverride('_method'));
-    app.use(flash());
     //seed the database 
    // seedDB();
     
@@ -43,8 +38,6 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
-    res.locals.error = req.flash('error');
-    res.locals.success = req.flash('success');
     next();
 });
 
